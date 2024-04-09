34613c435acb1a7d3839d187172dc7eca4ed4ad3
Julihermes Melo <julihermes@hotmail.com>
2024-04-09 19:58:15 -0300

    Fix focus when already open.

--- lua/sfm/extensions/sfm-focus.lua
+++ lua/sfm/extensions/sfm-focus.lua
@@ -11,7 +11,7 @@ function M.focus()
   end
 
   -- get current file path
-  -- local fpath = vim.api.nvim_buf_get_name(0)
+  local fpath = vim.api.nvim_buf_get_name(0)
   -- open explorer window
   -- M._view:open()
   api.explorer.close()
@@ -19,7 +19,7 @@ function M.focus()
   -- reload and render the explorer tree
   api.explorer.reload()
   -- focus the current file
-  -- api.navigation.focus(fpath)
+  api.navigation.focus(fpath)
 end
 
 function M.setup()
