56769aa45c9924affcc1e965e1ddbb1ce7bb4c09
Julihermes Melo <julihermes@hotmail.com>
2024-04-09 21:29:00 -0300

    Refactor.

--- lua/sfm/extensions/sfm-focus.lua
+++ lua/sfm/extensions/sfm-focus.lua
@@ -10,16 +10,18 @@ function M.focus()
     return
   end
 
-  -- get current file path
-  local fpath = vim.api.nvim_buf_get_name(0)
-  -- open explorer window
-  -- M._view:open()
   api.explorer.close()
-  api.explorer.open()
-  -- reload and render the explorer tree
-  api.explorer.reload()
-  -- focus the current file
-  api.navigation.focus(fpath)
+  api.explorer.toggle()
+  -- get current file path
+  -- local fpath = vim.api.nvim_buf_get_name(0)
+  -- -- open explorer window
+  -- -- M._view:open()
+  -- api.explorer.close()
+  -- api.explorer.open()
+  -- -- reload and render the explorer tree
+  -- api.explorer.reload()
+  -- -- focus the current file
+  -- api.navigation.focus(fpath)
 end
 
 function M.setup()
